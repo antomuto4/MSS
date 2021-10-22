b2796b3c3b27d0c4c4ca2af5770c598cba52b8d6
アントムト <56132390+antomuto4@users.noreply.github.com>
2021-10-22 19:06:02 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "src/include.h"
+int main()
+{
+    win::resolution(1280, 720);
+    
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
