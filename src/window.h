e16c399ae139343a2eb0e9116eed4ab850b861bb
antomuto4 <56132390+antomuto4@users.noreply.github.com>
2021-12-28 19:11:56 +0100

    Added: Camera namespace with setSize() function

--- src/window.h
+++ src/window.h
@@ -4,14 +4,14 @@
 sf::RenderWindow window(sf::VideoMode(1280, 720), "Window"); /*, sf::Style::Titlebar | sf::Style::Close);*/
 
 namespace win{
-    void Create(unsigned int X, unsigned int Y, sf::String title){
+    void resolution(unsigned int X, unsigned int Y){
         window.setSize(sf::Vector2u(X, Y));
-        window.setTitle(title);
     }
+}
 
-    sf::Vector2u size = window.getSize();
-    unsigned int width = size.x;
-    unsigned int height = size.y;
-
-
+namespace camera{
+	sf::View view;
+	void setSize(float x, float y){
+		view.setSize(x, y);
+	}
 }
