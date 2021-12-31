0e09dd0422080bb070e7cae4ac17ce458653e34c
antomuto4 <antomuto4@protonmail.com>
2021-12-31 16:29:25 +0100

    Added window.h: title on create

--- src/window.h
+++ src/window.h
@@ -4,8 +4,9 @@
 sf::RenderWindow window(sf::VideoMode(1280, 720), "Window"); /*, sf::Style::Titlebar | sf::Style::Close);*/
 
 namespace win{
-    void resolution(unsigned int X, unsigned int Y){
+    void resolution(unsigned int X, unsigned int Y, std::string Title){
         window.setSize(sf::Vector2u(X, Y));
+		window.setTitle(Title);
     }
 }
 
